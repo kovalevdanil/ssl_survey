96568e9a87a9dc9eb061f3ca03635249b298cb47
martin080 <opana.loog@gmail.com>
2020-08-10 22:32:19 +0300

    split scan_domain()

--- scan.c
+++ scan.c
@@ -10,8 +10,8 @@
 #define PORT "443"
 
 #define PROTO_COUNT 6
-static int proto_versions[PROTO_COUNT] = {SSL2_VERSION, SSL3_VERSION, TLS1_VERSION, TLS1_1_VERSION, TLS1_2_VERSION, TLS1_3_VERSION};
-static char *proto_names[PROTO_COUNT] = {"SSLv2", "SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3"};
+static int proto_versions[PROTO_COUNT] ={ SSL2_VERSION, SSL3_VERSION, TLS1_VERSION, TLS1_1_VERSION, TLS1_2_VERSION, TLS1_3_VERSION };
+static char *proto_names[PROTO_COUNT] ={ "SSLv2", "SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3" };
 
 BIO *BIO_create(SSL_CTX *ctx, char *hostname)
 {
@@ -52,13 +52,13 @@ SSL_CTX *ctx_create(const SSL_METHOD *method)
         exit(EXIT_FAILURE);
     }
 
-#ifdef TRUST_STORE
+    #ifdef TRUST_STORE
     if (!SSL_CTX_load_verify_locations(ctx, TRUST_STORE, NULL))
     {
         ERR_print_errors_fp(stderr);
         exit(EXIT_FAILURE);
     }
-#endif
+    #endif
 
     return ctx;
 }
@@ -81,14 +81,19 @@ int domain_check(char *domain)
     return 0;
 }
 
+void create_tls_contexts(SSL_CTX **contexts);
+void check_protocol_versions(SSL_CTX **contexts, int *available_proto, char *domain);
+int min_proto(int *available_proto);
+int max_proto(int *available_proto);
+int pubkey_len(SSL_CTX *ctx, char *domain);
+int check_all_ciphers(SSL_CTX **contexts, int *available_proto, char *domain, strbuf_t *strbuf);
+
 strbuf_t *scan_domain(char *domain)
 {
     if (strstr(domain, "https://") != NULL)
         domain = strstr(domain, "https://") + 8;
 
-    BIO *bio = NULL;
-    SSL *ssl = NULL;
-    char buf[512] = {0};
+    char buf[512] ={ 0 };
     strbuf_t *strbuf = buf_create(2048);
     sprintf(buf, "-----\nHOSTNAME: %s\n", domain);
     buf_add(strbuf, buf);
@@ -99,18 +104,55 @@ strbuf_t *scan_domain(char *domain)
         return strbuf;
     }
 
-    SSL_CTX *contexts[PROTO_COUNT] = {0};
-    int available_proto[PROTO_COUNT] = {0};
+    SSL_CTX *contexts[PROTO_COUNT] ={ NULL };
+    int available_proto[PROTO_COUNT] ={ 0 };
 
-    // creating context for every protocol versions
+    create_tls_contexts(contexts);
+
+    check_protocol_versions(contexts, available_proto, domain);
+
+    int min_ind = min_proto(available_proto);
+    int max_ind = max_proto(available_proto);
+
+    if (min_ind == -1)
+    {
+        buf_add(strbuf, "Error making connections\n");
+        goto end;
+    }
+
+    int bits = pubkey_len(contexts[max_ind], domain);
+
+    int bytes = sprintf(buf, "MIN TLS VERSION: %s\nMAX TLS VERSION: %s\nPUBKEY SIZE (in bits): %d\n",
+        proto_names[min_ind], proto_names[max_ind], bits);
+    buf[bytes] = '\0';
+
+    buf_add(strbuf, buf);
+
+    // checking all available ciphers for every tls version
+    check_all_ciphers(contexts, available_proto, domain, buf);
+
+    end:
+    for (int i = 0; i < PROTO_COUNT; i++)
+    {
+        SSL_CTX_free(contexts[i]);
+    }
+
+    return strbuf;
+}
+
+void create_tls_contexts(SSL_CTX **contexts)
+{
     for (int i = 0; i < PROTO_COUNT; i++)
     {
         contexts[i] = ctx_create(TLS_method());
         SSL_CTX_set_min_proto_version(contexts[i], proto_versions[i]);
         SSL_CTX_set_max_proto_version(contexts[i], proto_versions[i]);
     }
+}
 
-    // checking all protocol versions
+void check_protocol_versions(SSL_CTX **contexts, int *available_proto, char *domain)
+{
+    BIO *bio = NULL;
     for (int i = 0; i < PROTO_COUNT; i++)
     {
         bio = BIO_create_and_connect(contexts[i], domain);
@@ -123,8 +165,10 @@ strbuf_t *scan_domain(char *domain)
         BIO_free_all(bio);
         bio = NULL;
     }
+}
 
-    // search of min and max protocol versions
+int min_max_protocols(int *available_proto, int *min, int *max)
+{
     int min_ind = 0, max_ind = PROTO_COUNT - 1;
     while (min_ind < PROTO_COUNT && available_proto[min_ind] == 0)
         min_ind++;
@@ -134,32 +178,55 @@ strbuf_t *scan_domain(char *domain)
 
     if (min_ind == PROTO_COUNT)
     {
-        buf_add(strbuf, "Error making connections\n");
-        goto end;
+        return -1;
     }
 
-    // geting pubkey length
+    *min = min_ind;
+    *max = max_ind;
+}
+
+int min_proto(int *available_proto)
+{
+    int min_ind = 0;
+    while (min_ind < PROTO_COUNT && available_proto[min_ind] == 0)
+        min_ind++;
+    
+    return min_ind == PROTO_COUNT ? -1 : min_ind;
+}
+
+int max_proto(int *available_proto)
+{
+    int max_ind = PROTO_COUNT - 1;
+    while (max_ind >= 0 && available_proto[max_ind] == 0)
+        max_ind--;
+    
+    return max_ind;
+}
+
+int pubkey_len(SSL_CTX *ctx, char *domain)
+{
     X509 *cert;
+    SSL *ssl;
+    BIO *bio;
     int bits = 0;
 
-    bio = BIO_create_and_connect(contexts[max_ind], domain);
+    bio = BIO_create_and_connect(ctx, domain);
     BIO_get_ssl(bio, &ssl);
     cert = SSL_get_peer_certificate(ssl);
     bits = EVP_PKEY_bits(X509_get_pubkey(cert));
 
     X509_free(cert);
     BIO_shutdown_wr(bio);
-    BIO_free_all(bio);
-    bio = NULL;
 
-    int bytes = sprintf(buf, "MIN TLS VERSION: %s\nMAX TLS VERSION: %s\nPUBKEY SIZE (in bits): %d\n",
-                proto_names[min_ind], proto_names[max_ind], bits);
-    buf[bytes] = '\0';
-  
-    buf_add(strbuf, buf);
+    return bits;
+}
 
-    // checking all available ciphers for every tls version
+int check_all_ciphers(SSL_CTX **contexts, int *available_proto, char *domain, strbuf_t *strbuf)
+{
+    BIO *bio;
+    SSL *ssl;
     STACK_OF(SSL_CIPHER) * ciphers;
+    char buf[312];
 
     for (int i = 0; i < PROTO_COUNT; i++)
     {
@@ -194,12 +261,4 @@ strbuf_t *scan_domain(char *domain)
             ssl = NULL;
         }
     }
-
-end:
-    for (int i = 0; i < PROTO_COUNT; i++)
-    {
-        SSL_CTX_free(contexts[i]);
-    }
-
-    return strbuf;
 }
